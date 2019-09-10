79d95cc0c66c066b416f30cc0639329ea1afafd3
Josia Rodriguez <josiarodriguez@gmail.com>
2019-09-10 17:11:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+function theBeatlesPlay(musician, instruments){
+  let music = [];
+
+  for(let i = 0; i < musician.length; i++){
+    
+    music.push(`${musician[i]} + ${instruments[i]}`)
+  }
+}
