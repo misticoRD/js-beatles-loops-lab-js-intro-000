59a209855e32af2d1d409824e6163c7a68b9ad9d
Josia Rodriguez <josiarodriguez@gmail.com>
2019-09-10 17:11:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function theBeatlesPlay(musician, instruments){
   let music = [];
 
   for(let i = 0; i < musician.length; i++){
-    
+
     music.push(`${musician[i]} + ${instruments[i]}`)
   }
 }
